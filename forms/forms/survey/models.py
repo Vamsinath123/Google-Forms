52290b2a39310ab595d70d52953b69b6fc9af69f
vamsinath123 <ayyapu.vamsinath007@gmail.com>
2019-11-07 02:44:38 +0530

    terminal client

--- forms/forms/survey/models.py
+++ forms/forms/survey/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class data(models.Model):
+	username1 = models.CharField(max_length=150)
+	ans1 = models.CharField(max_length=200)
+	ans2 = models.CharField(max_length=200)
+	ans3 = models.CharField(max_length=200)
 # Create your models here.
